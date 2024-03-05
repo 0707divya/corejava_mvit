aaf62587f8bd5aca96abd063628dc5a364df2830
GAYATHRI S <GAYATHRI S@LAPTOP-FT4GB64P>
2024-03-05 15:49:29 +0530

    this is my java program

---
+++ corejava_mvit/src/corejava_mvitdivi/SampleJava.java
@@ -0,0 +1,17 @@
+package corejava_mvitdivi;
+
+import java.util.*;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		int a,b,c;
+		System.out.println("Enter the aand b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is :"+c);
+	}
+
+}
